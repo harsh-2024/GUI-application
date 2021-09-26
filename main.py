1be1c51544639108d233b6f7c4b820b5ec4daa6d
Harsh <tripathiharsh16@gmail.com>
2021-09-26 11:46:52 +0530

    convertor button placed and labels added

--- main.py
+++ main.py
@@ -5,10 +5,24 @@ window = Tk()     # Tk class for creating object window
 window.minsize(width=400, height=300)  # minsize method
 window.title("Convertor GUI Program")  # title method
 
-my_label = Label(text="Enter the distance in Miles")
+my_label = Label(text="Enter the distance in Miles : ")
 my_label.place(x=0, y=0)
 
-my_button = Button()
+
+
+input = Entry()
+input_text = input.get()
+input.place(x=201)
+
+def button_clicked():
+    
+
+my_button = Button(text="Convert", command=button_clicked)
+my_button.place(x=200, y=30)
+
+
+
+
 
 
 
