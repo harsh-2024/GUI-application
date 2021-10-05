3b10c660c3c018a2f47253c1dd40ef904ef29762
Harsh <tripathiharsh16@gmail.com>
2021-10-06 00:52:46 +0530

    final UI changes applied

--- main.py
+++ main.py
@@ -3,39 +3,14 @@ from tkinter import *
 
 window = Tk()
 
-# def delete():
-#     if active1 == True:
-#         info_label.destroy()
-#     if active2 == True:
-#         ver_label.destroy()
 
-
-
-
-def menu_callback():
-    print("I'm in the menu callback!")
-def submenu_callback():
-    print("I'm in the submenu callback!")
-
-
-# def info():
-#      global info_label
-#      global active1
-#      active1 = True
-#      info_label = Label(text="Name: Harsh Tripathi ")
-#      info_label.pack()
-#
-# def ver():
-#     global ver_label
-#     global active2
-#     active2 = True
-#     ver_label = Label(text="GUI Version : 2021.10.166")
-#     ver_label.pack()
 def hide_frames():
     distance_frame.pack_forget()
     currency_frame.pack_forget()
     length_frame.pack_forget()
     weight_frame.pack_forget()
+    gui_ver_frame.pack_forget()
+    dev_info_frame.pack_forget()
 
 
 def dist_calc():
@@ -100,7 +75,12 @@ def weight_tab_labels():
     cur_calc_button = Button(weight_frame, text="Convert to Kilogram", command=wt_calc).place(x=120,y=100)
 
 
+def gui_tab_labels():
+    my_label = Label(gui_ver_frame, text=" GUI Version : 2021.10.166").place(x=200, y=50)
 
+
+def dev_tab_labels():
+    my_label = Label(dev_info_frame, text=" Harsh Tripathi ").place(x=200, y=50)
 
 
 
@@ -127,12 +107,22 @@ def weight_frame_fun():
     weight_frame.pack(fill="both", expand=1)
     weight_tab_labels()
 
+def gui_frame_fun():
+    hide_frames()
+    gui_ver_frame.pack(fill="both", expand=1)
+    gui_tab_labels()
+
+def dev_frame_fun():
+    hide_frames()
+    dev_info_frame.pack(fill="both", expand=1)
+    dev_tab_labels()
+
+
 
 
 
 window.minsize(width=600, height=400)    # minsize method
 window.title("Convertor GUI Program")  # title method
-
 
 menu_widget = tkinter.Menu(window)
 submenu_widget = tkinter.Menu(menu_widget, tearoff=False)
@@ -144,94 +134,26 @@ submenu_widget.add_command(label="Weight", command=weight_frame_fun)
 
 
 menu_widget.add_cascade(label="Conversion", menu=submenu_widget)
-menu_widget.add_command(label="Factors", command=menu_callback)
-menu_widget.add_command(label="GUI Version", command=menu_callback)
-menu_widget.add_command(label="Developer Info",command=menu_callback)
+menu_widget.add_command(label="GUI Version", command=gui_frame_fun)
+menu_widget.add_command(label="Developer Info",command=dev_frame_fun)
 
 
 
 window.config(menu=menu_widget)
 
-distance_frame = Frame(width=600, height=400, bg="red")
-currency_frame = Frame(width=600, height=400, bg="blue")
-length_frame= Frame(width=600, height=400, bg="purple")
-weight_frame = Frame(width=600, height=400,)
+distance_frame = Frame(width=600, height=400, bg="#4c5670")
+currency_frame = Frame(width=600, height=400, bg="#4c5670")
+length_frame= Frame(width=600, height=400, bg="#4c5670")
+weight_frame = Frame(width=600, height=400, bg="#4c5670")
+gui_ver_frame = Frame(width=600, height=400, bg="#b3b8c7")
+dev_info_frame = Frame(width=600, height=400, bg="#b3b8c7")
 
 
 window.minsize(width=600, height=400)  # minsize method
 window.title("Convertor GUI Program")  # title method
 
 
-# delete_button = Button(text="Delete the text", command=delete)
-# delete_button.place(x=450, y=330)
 
-
-
-# my_label1 = Label(text="Enter the distance in Miles : ")
-# my_label1.place(x=0, y=0)
-#
-# my_label2 = Label(text="Enter the currency in dollars : ")
-# my_label2.place(x=0, y=70)
-#
-# my_label3_0 = Label(text="Know Your Body Mass Index here ")
-# my_label3_0.place(x=0, y=130)
-#
-# my_label3_1 = Label(text="Enter your height in CM : ")
-# my_label3_1.place(x=0, y=160)
-#
-# my_label3_2 = Label(text="Enter your weight in KG : ")
-# my_label3_2.place(x=0, y=180)
-#
-#
-#
-# input1 = Entry()
-# input1.place(x=201)
-#
-# input2 = Entry()
-# input2.place(x=201,  y=70)
-#
-# input3_1 = Entry()
-# input3_1.place(x=201, y=160)
-#
-# input3_2 = Entry()
-# input3_2.place(x=201, y=180)
-#
-# def distance():
-#             input_text = input1.get()
-#             value = float(input_text)
-#             km_result = value*1.609
-#             result_label = Label(text=f"{value} miles equals {km_result} Kilometers")
-#             result_label.place(x=250, y=30)
-#
-# def fun2():
-#             input_text = input2.get()
-#             value = int(input_text)
-#             rupee = value*74.27
-#             result_label = Label(text=f"{value} equals {rupee} USD")
-#             result_label.place(x=250, y=100)
-#
-#
-#
-# def fun3():
-#             input_text_1 = input3_1.get()
-#             input_text_2 = input3_2.get()
-#             value1 = float(input_text_1)
-#             value2 = float(input_text_2)
-#             bmi = round(value1/(value2*value2), 4)
-#             result_label = Label(text=f"Your BMI is {bmi} CM/KG^2")
-#             result_label.place(x=210, y=210)
-#
-#
-#
-#
-# my_button1 = Button(text="Convert to Kilometers", command=fun1)
-# my_button1.place(x=120, y=30)
-#
-# my_button2 = Button(text="Convert to rupees : ", command=fun2)
-# my_button2.place(x=120, y=100)
-#
-# my_button3 = Button(text="Calculate : ", command=fun3)
-# my_button3.place(x=130, y=210)
 
 
 
